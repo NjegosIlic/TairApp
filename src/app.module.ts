8835a9fdf0c43e649b653d75d7e1296958cdc832
Njegos Ilic <njegos.ilic1986@gmail.com>
2022-10-29 20:59:11 +0200

    Implement the mechanis to add anb edit administrators

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
+import { AppController } from './controlers/app.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseCofiguration } from 'config/database.configuration';
-
-
 import { Administrator } from 'entities/administrator.entity';
 import { AdministratorService } from './services/administrator/administrator.service';
 import { ArticleFeature } from 'entities/article-feature.entity';
@@ -16,6 +14,7 @@ import { Feature } from 'entities/feature.entity';
 import { Order } from 'entities/order.entity';
 import { Photo } from 'entities/photo.entity';
 import { User } from 'entities/user.entity';
+import { AdministratorController } from './controlers/api/administrator.controler';
 
 @Module({
   imports: [
@@ -42,7 +41,10 @@ import { User } from 'entities/user.entity';
     }),
     TypeOrmModule.forFeature([ Administrator ])
   ],
-  controllers: [AppController],
+  controllers: [
+    AppController,
+    AdministratorController,
+  ],
   providers: [AdministratorService],
 })
 export class AppModule {}
--- src/services/administrator/administrator.service.ts
+++ src/services/administrator/administrator.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Administrator } from 'entities/administrator.entity';
+import { AddAdministratorDto } from 'src/dtos/administrator/add.administrator.dto';
+import { EditAdministratorDto } from 'src/dtos/administrator/edit.administrator.dto';
 import { PrimaryGeneratedColumn, Repository } from 'typeorm';
 
 @Injectable()
@@ -19,6 +21,42 @@ export class AdministratorService {
             where: {administratorId:id}
         })
     }     
+
+    add(data: AddAdministratorDto): Promise<Administrator> {
+        //DTO -> Model
+        //username -> username
+        //password -[...]-> passwordHash ! Stvar izbora! SHA512
+
+        const crypto = require ('crypto');
+
+        const passwordHash = crypto.createHash('sha512');
+        passwordHash.update(data.password);
+
+        const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+        let newAdmin: Administrator = new Administrator();
+        newAdmin.username = data.username;
+        newAdmin.passwordHash = passwordHashString;
+
+        return this.administrator.save(newAdmin);
+    }
+
+    async editById(id: number, data: EditAdministratorDto): Promise<Administrator> {
+        let admin: Administrator = await this.administrator.findOne({
+            where: {administratorId:id}
+        });
+
+        const crypto = require ('crypto');
+
+        const passwordHash = crypto.createHash('sha512');
+        passwordHash.update(data.password);
+
+        const passwordHashString = passwordHash.digest('hex').toUpperCase();
+
+        admin.passwordHash = passwordHashString;
+
+        return this.administrator.save(admin);
+    }
 }
 
 
